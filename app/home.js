48385204457e774c82f39c85482382754f67f6bf
Salisu Aminu Abdurrahman <30727466+salicode@users.noreply.github.com>
2025-09-03 11:45:56 +0100

    Update home.js

--- app/home.js
+++ app/home.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { SafeAreaView, ScrollView, View } from "react-native";
 import { COLORS, SIZES } from "../constants/theme";
-
+import ScreenHeaderBtn from "../components/ScreenHeaderBtn"
 const Home = () => {
 
   const [userDetails, setUserDetails] = useState(null);
@@ -17,7 +17,21 @@ const Home = () => {
   };
     return (
      <>
+     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+     <ScreenHeaderBtn/>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+          testID="screensDisplay"
+        >
 
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
      </>
     );
   };
